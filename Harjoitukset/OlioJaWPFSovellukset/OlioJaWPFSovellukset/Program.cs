5875995d9411db670ee1eb9b4e97be08ff88d500
Olli <juhola.olli@gmail.com>
2021-01-15 11:42:27 +0200

    testi

--- Harjoitukset/OlioJaWPFSovellukset/OlioJaWPFSovellukset/Program.cs
+++ Harjoitukset/OlioJaWPFSovellukset/OlioJaWPFSovellukset/Program.cs
@@ -6,7 +6,7 @@ namespace OlioJaWPFSovellukset
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
