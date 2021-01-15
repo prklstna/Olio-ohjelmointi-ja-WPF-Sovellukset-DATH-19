5d973094b1891adddf3fb2d5ebf7dae3a6980f37
Olli <juhola.olli@gmail.com>
2021-01-15 11:35:45 +0200

    Projekti Commit

---
+++ Harjoitukset/OlioJaWPFSovellukset/OlioJaWPFSovellukset/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlioJaWPFSovellukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
