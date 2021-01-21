0585d765b83ae46a36ca70c9ffcd425f53172654
prklstna <70888266+prklstna@users.noreply.github.com>
2021-01-21 11:57:55 +0200

    Harjoitus tehty

---
+++ Harjoitukset/OlioJaWPF/Harjoitus1/Ajoneuvo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus1
+{
+    class Ajoneuvo
+    {
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvo");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Nopeus: " + Nopeus);
+            Console.WriteLine("-- Renkaat: " + Renkaat);
+        }
+
+
+        public string ToString()
+        {
+            string merkkijono = "Ajoneuvo: " + Nimi + "Nopeus: " + Nopeus " km/h. Renkaat: " + Renkaat + " kpl";
+
+            return merkkijono;
+        }
+    }
+
+
+}
---
+++ Harjoitukset/OlioJaWPF/Harjoitus1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Harjoitus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto = new Ajoneuvo();
+
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+
+            auto.TulostaData();
+
+            string autonTiedot = auto.ToString();
+            Console.WriteLine(autonTiedot);
+
+            Ajoneuvo mopo = new Ajoneuvo();
+            
+            mopo.Nimi = "Skootteri";
+            mopo.Nopeus = 55;
+            mopo.Renkaat = 2;
+
+            mopo.TulostaData();
+            Console.WriteLine(mopo.ToString());
+
+
+        }
+    }
+}
