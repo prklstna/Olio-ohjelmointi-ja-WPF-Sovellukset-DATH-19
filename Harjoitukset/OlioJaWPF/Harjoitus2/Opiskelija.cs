7ee8f8261ffd2cf89cf88d98d6a623779b1e1dfa
prklstna <70888266+prklstna@users.noreply.github.com>
2021-01-21 12:16:25 +0200

    Harjoitus2 Commits

---
+++ Harjoitukset/OlioJaWPF/Harjoitus2/Opiskelija.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Harjoitus2
+{
+    class Opiskelija
+    {
+
+        private string Nimi { get; set; }
+        private string OpiskelijaID { get; set; }
+        private int Opintopisteet { get; set; }
+
+        public Opiskelija(string _nimi, string _id, int _op)
+        {
+            Nimi = _nimi;
+            OpiskelijaID = _id;
+            Opintopisteet = _op;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Opiskelija: ");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Opiskelijan ID: " + OpiskelijaID);
+            Console.WriteLine("-- Opintopisteet: " + Opintopisteet);
+            Console.WriteLine("");
+        }
+
+        public void MuokkaaOpintopisteitä(int i)
+        {
+            Opintopisteet += i;
+
+            if (Opintopisteet < 0)
+            {
+                Opintopisteet = 0;
+            }
+        }
+    }
+}
---
+++ Harjoitukset/OlioJaWPF/Harjoitus2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Harjoitus2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Matti", "TVTPT19s", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}
